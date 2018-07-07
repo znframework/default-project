e178e71092cfe4b6b9945c21ba20b73ed68c719b
Ozan Uykun <znframework@gmail.com>
2018-07-07 16:42:47 +0300

    Modified opening page.

---
+++ ClassMap.php
@@ -0,0 +1,8 @@
+<?php
+#----------------------------------------------------------------------
+# This file automatically created and updated
+#----------------------------------------------------------------------
+$classMap['classes']['project\controllers\home'] = 'Projects/Frontend/Controllers/Home.php';
+$classMap['classes']['project\controllers\initialize'] = 'Projects/Frontend/Controllers/Initialize.php';
+$classMap['namespaces']['project\controllers\home'] = 'home';
+$classMap['namespaces']['project\controllers\initialize'] = 'initialize';
